4f536be24e659a2fe719454c51d8909e47c34d77
20fut <231612@astanait.edu.kz>
2024-11-04 14:56:55 +0500

    generate hash

--- backend/hash-service/src/controller/delete-hash.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from 'express';
-import { DeleteHashCommand } from '../services/hashCommandFactory';
-
-export const deleteHash = async (req: Request, res: Response) => {
-  const command = new DeleteHashCommand();
-  await command.execute(req, res);
-};
-
