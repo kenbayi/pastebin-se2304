1bf28da59e6478880e74af0e3426e26455bff7f2
20fut <231612@astanait.edu.kz>
2024-11-04 21:16:35 +0500

    getHashCommand and getPasteIdCommand

--- backend/hash-service/src/services/hashCommandFactory.ts
+++ backend/hash-service/src/services/hashCommandFactory.ts
@@ -27,6 +27,29 @@ export class DeleteHashCommand implements Command {
     }
   }
 }
+export class GetHashCommand implements Command {
+  async execute(req: Request, res: Response): Promise<void> {
+    const pasteId = Number(req.params.pasteId);
+    try {
+      const hash = await hashFacade.getHash(pasteId);
+      res.status(200).json({ hash });
+    } catch (error) {
+      res.status(404).json({ error: "Failed to get hash" });
+    }
+  }
+}
+
+export class GetPasteIdCommand implements Command {
+  async execute(req: Request, res: Response): Promise<void> {
+    const { hash } = req.params;
+    try {
+      const pasteId = await hashFacade.getPasteId(hash);
+      res.status(200).json({ pasteId });
+    } catch (error) {
+      res.status(404).json({ error: "Failed to get paste id by hash" });
+    }
+  }
+}
 
 
 
